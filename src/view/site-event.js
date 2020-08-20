c7087f59208138b7b8d6aadb597ecd714daba028
StasRomanov <stasromanov801@gmail.com>
2020-08-20 16:33:57 +0300

    fix offers

--- src/view/site-event.js
+++ src/view/site-event.js
@@ -3,8 +3,8 @@ import Abstract from "./abstract";
 const createSiteEventTemplate = (bonusOption) => {
   const {name, price, used} = bonusOption;
   return `<div class="event__offer-selector">
-    <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" ${used ? `checked` : ``}>
-      <label class="event__offer-label" for="event-offer-luggage-1">
+    <input class="event__offer-checkbox  visually-hidden" id="event-offer-${name}" type="checkbox" name="event-offer-luggage" ${used ? `checked` : ``}>
+      <label class="event__offer-label" for="event-offer-${name}">
         <span class="event__offer-title">${name}</span>
         &plus;
         &euro;&nbsp;<span class="event__offer-price">${price}</span>
