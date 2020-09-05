6e7cb05f46a62f6d347ec8dd16fc59238297ee99
StasRomanov <stasromanov801@gmail.com>
2020-09-06 01:23:04 +0300

    filter (fix remove unused module)

--- src/main.js
+++ src/main.js
@@ -2,13 +2,9 @@ import {waypoints} from "./mock/data";
 import TravelDaysList from "./presenter/travel-day";
 import Header from "./presenter/header";
 import Waypoints from "./model/waypoints";
-import Filter from "./model/filter";
-import {FilterType} from "./const";
 
 const waypointModel = new Waypoints();
-const WaypointFilter = new Filter();
 waypointModel.setWaypoint(waypoints);
-WaypointFilter.setFilter(FilterType.DEFAULT);
 const header = new Header(waypointModel);
 const daysList = new TravelDaysList(waypointModel, header);
 
